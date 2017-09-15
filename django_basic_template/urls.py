aef91fe00cce8f2a549934cbc8d55cba8f2e7989
Bogusław Chojecki <b.chojecki90@gmail.com>
2017-09-15 05:10:03 +0200

    function signup

--- django_basic_template/urls.py
+++ django_basic_template/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from my_project import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^signup/$',
+        views.signup, name='signup'),
 ]
